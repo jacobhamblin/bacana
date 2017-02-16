636f04639691a929e54fe24205bc50f7cc54fe06
Jacob Hamblin <jacob@hamblin.cc>
2017-02-16 02:20:17 -0700

    update webpack, postcss config

--- webpack.config.js
+++ webpack.config.js
@@ -17,25 +17,24 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /\.js$/, loader: 'babel', exclude: /node_modules/,
+        test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/,
         query: { presets: ['es2015', 'react', 'stage-0'] }
       },
       { test: /\.json$/, loader: 'json-loader' },
-			{ test: /\.css$/, loader: 'css-loader!postcss-loader' },
-			{ test: /\.scss$/, loader: ExtractTextPlugin.extract('css!postcss-loader!sass') },
-			{ test: /\.png$/, loader: 'url-loader?prefix=img/&limit=5000' },
-			{ test: /\.jpg$/, loader: 'url-loader?prefix=img/&limit=5000' },
-			{ test: /\.gif$/, loader: 'url-loader?prefix=img/&limit=5000' },
-			{ test: /\.woff$/, loader: 'url-loader?prefix=font/&limit=5000' },
-			{ test: /\.woff2$/, loader: 'url-loader?prefix=font/&limit=5000' },
-			{ test: /\.eot$/, loader: 'file-loader?prefix=font/' },
-			{ test: /\.ttf$/, loader: 'file-loader?prefix=font/' },
-			{ test: /\.svg$/, loader: 'file-loader?prefix=font/' },
-			{ test: /\.txt$/, loader: 'raw-loader' },
+            { test: /\.css$/, loader: 'css-loader!postcss-loader' },
+            { test: /\.scss$/, loader: ExtractTextPlugin.extract('css-loader!postcss-loader!sass-loader') },
+            { test: /\.png$/, loader: 'url-loader?prefix=img/&limit=5000' },
+            { test: /\.jpg$/, loader: 'url-loader?prefix=img/&limit=5000' },
+            { test: /\.gif$/, loader: 'url-loader?prefix=img/&limit=5000' },
+            { test: /\.woff$/, loader: 'url-loader?prefix=font/&limit=5000' },
+            { test: /\.woff2$/, loader: 'url-loader?prefix=font/&limit=5000' },
+            { test: /\.eot$/, loader: 'file-loader?prefix=font/' },
+            { test: /\.ttf$/, loader: 'file-loader?prefix=font/' },
+            { test: /\.svg$/, loader: 'file-loader?prefix=font/' },
+            { test: /\.txt$/, loader: 'raw-loader' },
     ],
   },
   plugins: [
     new ExtractTextPlugin('../[name].css'),
   ],
-  postcss: [ autoprefixer({ browsers: '> 5%'}) ],
 };
